a0f7396bc5d29e803d1687c60faba63f3ad57909
Shaghik66 <kaprielianshaghig961@gmail.com>
2026-02-26 23:47:31 -0800

    Button Editing

--- src/components/ProductButtons/ProductButtons.jsx
+++ src/components/ProductButtons/ProductButtons.jsx
@@ -4,10 +4,10 @@ import "./ProductButtons.css"
 export default class ProductButtons extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { count: 0 };
-  }
+    this.state = { count: 0};
+}
 
-  increment() {
+increment() {
     this.setState({ count: ++this.state.count });
   }
 
@@ -22,9 +22,9 @@ export default class ProductButtons extends React.Component {
   render() {
     return (
       <>
-        <button onClick={() => this.decrement()} type="button" className="productButton">-</button>
+        <button onClick={() => this.decrement()} type="button" className="productButton">➖</button>
         <p>{this.state.count}</p>
-        <button onClick={() => this.increment()} type="button" className="productButton">+</button>
+        <button onClick={() => this.increment()} type="button" className="productButton">➕</button>
       </>
     );
   }
